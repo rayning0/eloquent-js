9eef610e7fa23839977046b8ee1f3d90bb90e2dc
Raymond Gan <rayning@yahoo.com>
2018-10-14 13:08:59 -0700

    arrayToList() handles array with 1 value

--- ch4/arraytolist2.test.js
+++ ch4/arraytolist2.test.js
@@ -4,9 +4,18 @@ function arrayToList(array) {
   if (array.length === 0) {
     return []
   }
+
+  return {
+    value : array[0],
+    rest  : null
+  }
 }
 describe('arrayToList()', () => {
   test('handles empty array', () => {
     expect(arrayToList([])).toEqual([])
+  })
+
+  test('handles array with 1 value', () => {
+    expect(arrayToList([5])).toEqual({ value: 5, rest: null })
   })
 })
