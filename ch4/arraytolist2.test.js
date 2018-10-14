b76c2cc173eac47332f8f89da4be0a1c1dc57d7b
Raymond Gan <rayning@yahoo.com>
2018-10-14 13:31:10 -0700

    Add 2 tests for arrayToList(): 1) Handle array with 1 value. 2) Handle array with multiple values.

--- ch4/arraytolist2.test.js
+++ ch4/arraytolist2.test.js
@@ -1,14 +1,18 @@
-// const array = [1, 5, 10, 15, 20, 30, 50, 55]
-
 function arrayToList(array) {
   if (array.length === 0) {
     return []
   }
-
-  return {
-    value : array[0],
+  const list = {
+    value : array[array.length - 1],
     rest  : null
   }
+
+  for (let i = array.length - 2; i >= 0; i--) {
+    const rest = Object.assign({}, list)
+    list.value = array[i]
+    list.rest = rest
+  }
+  return list
 }
 describe('arrayToList()', () => {
   test('handles empty array', () => {
@@ -17,5 +21,21 @@ describe('arrayToList()', () => {
 
   test('handles array with 1 value', () => {
     expect(arrayToList([5])).toEqual({ value: 5, rest: null })
+  })
+
+  test('handles array with multiple values', () => {
+    expect(arrayToList([1, 5, 10, 15])).toEqual({
+      value : 1,
+      rest  : {
+        value : 5,
+        rest  : {
+          value : 10,
+          rest  : {
+            value : 15,
+            rest  : null
+          }
+        }
+      }
+    })
   })
 })
