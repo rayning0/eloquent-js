016929153a006860ac9482e65e7e95324dfb443e
Raymond Gan <rayning@yahoo.com>
2018-10-17 09:56:00 -0700

    Finish recursive deepEqual(), with full tests and test output
    - Wrote many tests with objects up to 3 levels deep
    - Previous "for-in" loop passed all tests, but wasn't recursive and didn't check each bottom value
    - Add print statements for each key and value checked, all the way to bottom
    - Because of ESLint error about "for-in" loop, added Object.prototype.hasOwnProperty()
    - Delete --silent flag in launch.json, to allow console.log() output in Jest

--- ch4/deepEqual2.test.js
+++ ch4/deepEqual2.test.js
@@ -7,8 +7,8 @@
 
 function deepEqual(obj1, obj2) {
   // specifically check for "null" because typeof null === 'object'
-  if (obj1 === null || obj2 === null
-    || typeof obj1 !== 'object' || typeof obj2 !== 'object') {
+  if (obj1 === null || obj2 === null || typeof obj1 !== 'object' || typeof obj2 !== 'object') {
+    console.log(`val1: ${obj1}. val2: ${obj2}`)
     return obj1 === obj2
   }
 
@@ -26,12 +26,32 @@ function deepEqual(obj1, obj2) {
   // I use "for-in" instead of Object.keys.forEach()
   // because we can't break out of middle of forEach() loop!
   // "for-in" lets us leave with "return" statement before looping through all keys.
+
   for (const key in obj1) {
-    if (obj1[key] !== obj2[key]) {
-      return false
+    // Reason for Object.prototype.hasOwnProperty() here: https://stackoverflow.com/a/1963179/2175188
+    // And here: https://eslint.org/docs/rules/no-prototype-builtins
+    if (Object.prototype.hasOwnProperty.call(obj1, key)) {
+      console.log(`key: ${key}`)
+
+      // If key from obj1 is NOT in obj2, or deepEqual(val1, val2) is FALSE
+      if (!(key in obj2) || !deepEqual(obj1[key], obj2[key])) {
+        console.log('---FALSE---')
+        return false
+      }
     }
   }
+  console.log('---TRUE---')
   return true
+
+  // Strangely, all tests below pass with these 6 lines.
+  // But this doesn't dig into bottom of each object.
+
+  // for (const key in obj1) {
+  //   if (obj1[key] !== obj2[key]) {
+  //     return false
+  //   }
+  // }
+  // return true
 }
 
 describe('deepEqual()', () => {
@@ -47,17 +67,17 @@ describe('deepEqual()', () => {
   })
 
   test('if both objects are 1 level deep', () => {
-    const obj = { here: 1, is: 'an', object: true }
+    const obj = { here: 1, is: 'an', dog: true }
 
     // Copy obj by VALUE, not REFERENCE. It means if you change obj2, obj won't change.
     const obj2 = { ...obj } // or Object.assign({}, obj)
-    obj2.c = 5 // obj2 = { here: 1, is: 'an', object: true, c: 5 }
+    obj2.c = 5 // obj2 = { here: 1, is: 'an', dog: true, c: 5 }
 
-    // obj3 = { c: 5, here: 1, is: 'an', object: true }
+    // obj3 = { c: 5, here: 1, is: 'an', dog: true }
     const obj3 = Object.assign({ c: 5 }, obj)
 
-    const obj4 = { here: 1, is: 'cat', object: true }
-    const obj5 = { here: 1, is: 'an', object: false }
+    const obj4 = { here: 1, is: 'cat', dog: true }
+    const obj5 = { here: 1, is: 'an', dog: false }
 
     expect(deepEqual({}, {})).toEqual(true)
     expect(deepEqual({}, obj)).toEqual(false)
@@ -74,6 +94,129 @@ describe('deepEqual()', () => {
   })
 
   test('if both objects are 1 to multiple levels deep', () => {
+    const obj = { here: { is: 'an' }, dog: 2 }
 
+    // Deep copy of obj, down multiple levels. When copy is changed, original stays unchanged.
+    const obj2 = JSON.parse(JSON.stringify(obj))
+    obj2.here.is = 'blah' // { here: { is: 'blah' }, dog: 2 }
+    const obj3 = JSON.parse(JSON.stringify(obj))
+    obj3.dog = 3 // { here: { is: 'an' }, dog: 3 }
+    const obj4 = JSON.parse(JSON.stringify(obj))
+    obj4.here.is = { deeper: true } // { here: { is: { deeper: true }}, dog: 2 }
+    const obj5 = JSON.parse(JSON.stringify(obj4))
+    // obj5 = { here: { is: { deeper: true }}, dog: { deep: { deeper: 4 }}}
+    obj5.dog = { deep: { deeper: 4 } }
+    const obj6 = JSON.parse(JSON.stringify(obj5))
+    obj6.dog.deep.deeper = 5 // { here: { is: { deeper: true }}, dog: { deep: { deeper: 5 }}}
+
+    console.log('****** obj vs obj ******')
+    expect(deepEqual(obj, obj)).toEqual(true)
+    console.log('****** obj vs { here: 1, dog: 2 } ******')
+    expect(deepEqual(obj, { here: 1, dog: 2 })).toEqual(false)
+    console.log('****** obj vs obj2 ******')
+    expect(deepEqual(obj, obj2)).toEqual(false)
+    console.log('****** obj vs obj3 ******')
+    expect(deepEqual(obj, obj3)).toEqual(false)
+    console.log('****** obj vs obj4 ******')
+    expect(deepEqual(obj, obj4)).toEqual(false)
+    console.log('****** obj4 vs obj4 ******')
+    expect(deepEqual(obj4, obj4)).toEqual(true)
+    console.log('****** obj4 vs obj5 ******')
+    expect(deepEqual(obj4, obj5)).toEqual(false)
+    console.log('****** obj5 vs obj5 ******')
+    expect(deepEqual(obj5, obj5)).toEqual(true)
+    console.log('****** obj5 vs obj6 ******')
+    expect(deepEqual(obj5, obj6)).toEqual(false)
   })
 })
+
+// Output of last block of tests:
+
+// ****** obj vs obj ******
+// key: here
+// key: is
+// val1: an.val2: an
+// --- TRUE ---
+// key: dog
+// val1: 2. val2: 2
+// --- TRUE ---
+
+// ****** obj vs { here: 1, dog: 2 } ******
+// key: here
+// val1: [object Object].val2: 1
+// --- FALSE ---
+
+// ****** obj vs obj2 ******
+// key: here
+// key: is
+// val1: an.val2: blah
+// --- FALSE ---
+// --- FALSE ---
+
+// ****** obj vs obj3 ******
+// key: here
+// key: is
+// val1: an.val2: an
+// --- TRUE ---
+// key: dog
+// val1: 2. val2: 3
+// --- FALSE ---
+
+//   ****** obj vs obj4 ******
+// key: here
+// key: is
+// val1: an.val2: [object Object]
+// --- FALSE ---
+// --- FALSE ---
+
+// ****** obj4 vs obj4 ******
+// key: here
+// key: is
+// key: deeper
+// val1: true.val2: true
+// --- TRUE ---
+// --- TRUE ---
+// key: dog
+// val1: 2. val2: 2
+// --- TRUE ---
+
+// ****** obj4 vs obj5 ******
+// key: here
+// key: is
+// key: deeper
+// val1: true.val2: true
+// --- TRUE ---
+// --- TRUE ---
+// key: dog
+// val1: 2. val2: [object Object]
+// --- FALSE ---
+
+// ****** obj5 vs obj5 ******
+// key: here
+// key: is
+// key: deeper
+// val1: true.val2: true
+// --- TRUE ---
+// --- TRUE ---
+// key: dog
+// key: deep
+// key: deeper
+// val1: 4. val2: 4
+// --- TRUE ---
+// --- TRUE ---
+// --- TRUE ---
+
+// ****** obj5 vs obj6 ******
+// key: here
+// key: is
+// key: deeper
+// val1: true.val2: true
+// --- TRUE ---
+// --- TRUE ---
+// key: dog
+// key: deep
+// key: deeper
+// val1: 4. val2: 5
+// --- FALSE ---
+// --- FALSE ---
+// --- FALSE ---
